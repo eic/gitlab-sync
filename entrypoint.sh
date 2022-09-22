604d29231fd0a9a340ce2793edc2cc8e0575975e
Urs Roesch <github@bun.ch>
2022-09-22 02:54:27 +0200

    use fetch all

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${I
 
 case "${GITHUB_EVENT_NAME}" in
     push|create|pull_request)
-        git branch
+        git fetch --all
         git push -f --all target
         git push -f --prune target
         git push -f --tags target
