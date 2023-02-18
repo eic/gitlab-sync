85b477008cf8d0b5dac966430450d29798ee611b
Urs Roesch <github@bun.ch>
2023-02-18 12:45:40 +0100

    Refactor for forking

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,30 @@
+#!/usr/bin/env bash
+
 set -x
 echo "${GITHUB_EVENT_NAME}"
-git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${INPUT_TARGET_URL#https://}
+
+function git-setup() {
+  printf -v url "https://%s:%s@%s" \
+    "${INPUT_TARGET_USERNAME}" \
+    "${INPUT_TARGET_TOKEN}" \
+    "${INPUT_TARGET_URL#https://}"
+  git remote add target "${url}"
+}
+
+git-setup
 
 case "${GITHUB_EVENT_NAME}" in
-    push|create|pull_request)
-        git fetch --all
-        git push -f --all target
-        git push -f --prune target
-        git push -f --tags target
-        ;;
-    delete)
-        git push -d target ${GITHUB_EVENT_REF}
-        ;;
-    *)
-        break
-        ;;
+push|create|pull_request)
+  git fetch --all
+  git push -f --all target
+  git push -f --prune target
+  git push -f --tags target
+    ;;
+delete)
+  git push -d target ${GITHUB_EVENT_REF}
+    ;;
+*)
+  break
 esac
+
+# vim:
