04d8559d74e2373545f9558b015bde3b88bb46ab
Wouter Deconinck <wdconinc@gmail.com>
2025-07-21 13:15:47 -0500

    Target (#2)
    This PR allows for specifying the git remote name to allow reuse of the
    action.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ function git-setup() {
     "${INPUT_TARGET_USERNAME}" \
     "${INPUT_TARGET_TOKEN}" \
     "${INPUT_TARGET_URL#https://}"
-  echo "git remote add target ${url}"
-  git remote add target "${url}"
+  echo "git remote add ${INPUT_REMOTE} ${url}"
+  git remote add ${INPUT_REMOTE} "${url}"
   set -x
 }
 
@@ -17,13 +17,13 @@ case "${GITHUB_EVENT_NAME}" in
 push|create|pull_request|workflow_dispatch)
   git-setup
   git fetch --all
-  git push -f ${INPUT_CISKIP:+-o ci.skip} --all target
-  git push -f --prune target
-  git push -f --tags target
+  git push -f ${INPUT_CISKIP:+-o ci.skip} --all ${INPUT_REMOTE}
+  git push -f --prune ${INPUT_REMOTE}
+  git push -f --tags ${INPUT_REMOTE}
     ;;
 delete)
   git-setup
-  git push -d target "${GITHUB_EVENT_REF}"
+  git push -d ${INPUT_REMOTE} "${GITHUB_EVENT_REF}"
     ;;
 *)
   #break
