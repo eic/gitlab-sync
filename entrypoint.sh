c948138bb4f7e5aedf4f7e12b348a46e6aceadf6
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-01-09 11:45:11 -0500

    Merge `git push --all` and `git push --prune` steps.
    `git push --prune` tries to use default push configuration, which doesn't work when HEAD is not on a branch. One fix could be to specify refs that prune is supposed to push, but we already have a push for `--all`, so why not combine the operations.

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,7 @@ case "${GITHUB_EVENT_NAME}" in
 push|create|pull_request|workflow_dispatch)
   git-setup
   git fetch --all
-  git push -f ${INPUT_CISKIP:+-o ci.skip} --all ${INPUT_REMOTE}
-  git push -f --prune ${INPUT_REMOTE}
+  git push -f ${INPUT_CISKIP:+-o ci.skip} --all --prune ${INPUT_REMOTE}
   git push -f --tags ${INPUT_REMOTE}
     ;;
 delete)
